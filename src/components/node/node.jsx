4dc2d1aed896df7323a3b3fcbe8a02df8aa307e8
hitler <hitler0504@gmail.com>
2016-10-18 11:35:36 +0800

    submit

--- src/components/node/node.jsx
+++ src/components/node/node.jsx
@@ -6,18 +6,15 @@ import "./index.scss";
 
 import Common from "../../common";
 
-
-
-
 export default class Node extends Component {
   constructor(props) {
     super(props);
   }
 
-  onClick() { 
-     this.props.history.replaceState(null, '/about');
+  onClick() {
+    this.props.history.replaceState(null, '/about');
   }
-  
+
   render() {
     let nodeInfo = this.props.nodeInfo;
     return (
@@ -26,11 +23,9 @@ export default class Node extends Component {
         <div className="host-item"><span className="title">IP: </span>{nodeInfo.status.addresses[0].address}</div>
         <div className="host-item"><span className="title">系统: </span>{nodeInfo.status.nodeInfo.osImage}</div>
         <div className="host-item"><span className="title">cpu: </span>{nodeInfo.status.capacity.cpu}</div>
-        <div className="host-item"><span className="title">内存: </span>{Common.formatToMb(nodeInfo.status.capacity.memory.replace("Ki", "")) }</div>
+        <div className="host-item"><span className="title">内存: </span>{Common.formatToMb(nodeInfo.status.capacity.memory.replace("Ki", ""))}</div>
         <div className="host-item"><span className="title">Pod最大数: </span>{nodeInfo.status.allocatable.pods}</div>
 
-        
-        
         <div className="host-item"><span className="title">Pod当前数: </span>
           {this.props.podSet[nodeInfo.status.addresses[0].address]}
         </div>
